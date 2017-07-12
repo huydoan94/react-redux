b8650ed0ccb6c8fdc6d9ccd91732cea751b0d024
huydoan <Quynh@1993>
2017-07-12 17:50:15 +0700

    Fix some bugs

--- client/index.js
+++ client/index.js
@@ -3,10 +3,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import { browserHistory, Router } from 'react-router';
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore, compose } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { AppRoute } from './routes';
 import { AppReducer } from './reducers';
-// import { apiMiddleware } from './middlewares';
 import thunk from 'redux-thunk';
 
 const initStore = (reducers, state) => {
--- karma.conf.js
+++ karma.conf.js
@@ -1,45 +1,63 @@
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = function (config) {
     config.set({
-        preprocessors: {
-            'client/**/*.spec.js': ['webpack', 'sourcemap']
-        },
-        webpack: {
-            devtool: 'inline-source-map',
-            module: {
-                loaders: [{
-                    test: /\.js$/,
-                    loader: 'babel',
-                    exclude: /node_modules/,
-                    query: {
-                        plugins: ['transform-decorators-legacy'],
-                        presets: ['es2015', 'es2017', 'react', 'stage-0']
-                    }
-                },
-                { test: /\.scss$/, loader: 'ignore' },
-                { test: /\.css$/, loader: 'ignore' },
-                { test: /\.png$/, loader: 'ignore' },
-                { test: /\.json$/, loader: 'json' }
-                ]
-            },
-            externals: {
-                'cheerio': 'window',
-                'react/addons': true,
-                'react/lib/ExecutionEnvironment': true,
-                'react/lib/ReactContext': true
-            }
-        },
-        webpackServer: {
-            noInfo: true
-        },
-        browsers: ['PhantomJS'],
-        frameworks: ['mocha'],
-        reporters: ['dots'],
         files: [
             './node_modules/phantomjs-polyfill-find/find-polyfill.js',
             './node_modules/phantomjs-polyfill-includes/includes-polyfill.js',
             './node_modules/phantomjs-polyfill-string-includes/index.js',
             './node_modules/babel-polyfill/dist/polyfill.js',
             'client/**/*.spec.js'
-        ]
+        ],
+        preprocessors: {
+            'client/**/*.spec.js': 'webpack',
+        },
+        webpack: {
+            devtool: 'inline-source-map',
+            module: {
+                loaders: [
+                    {
+                        test: /\.js$/,
+                        loader: 'babel',
+                        exclude: /node_modules/
+                    },
+                    // {
+                    //     test: /\.js$|^\.spec.js$/,
+                    //     loader: 'istanbul-instrumenter-loader',
+                    //     include: path.join(__dirname, '/client'),
+                    //     exclude: /node_modules/
+                    // },
+                    { test: /\.scss$/, loader: 'ignore' },
+                    { test: /\.css$/, loader: 'ignore' },
+                    { test: /\.png$/, loader: 'ignore' },
+                    { test: /\.json$/, loader: 'json' }
+                ]
+            },
+            externals: {
+                cheerio: 'window',
+                'react/addons': true,
+                'react/lib/ExecutionEnvironment': true,
+                'react/lib/ReactContext': true
+            },
+            resolve: {
+                root: [path.resolve('./client')]
+            },
+            plugins: [
+                new webpack.optimize.OccurenceOrderPlugin(),
+                new webpack.optimize.DedupePlugin(),
+                new webpack.NoErrorsPlugin()
+            ]
+        },
+        webpackServer: {
+            noInfo: true
+        },
+        browsers: ['PhantomJS'],
+        frameworks: ['mocha'],
+        reporters: ['progress', 'coverage-istanbul'],
+        coverageIstanbulReporter: {
+            reports: ['text-summary'],
+            fixWebpackSourcePaths: true
+        }
     });
 };
